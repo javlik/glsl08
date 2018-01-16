6a9870fb2c64e0a7af9ee9c7f51c981232ad955a
javlik <jiri.havlik78@gmail.com>
2018-01-16 23:35:38 +0100

    Merge branch 'master' of https://github.com/javlik/glsl08
    # Conflicts:
    #	app/src/main/java/cz/javlik/glsl08/MainActivity.java

--- app/src/main/java/cz/javlik/glsl08/MainActivity.java
+++ app/src/main/java/cz/javlik/glsl08/MainActivity.java
@@ -241,7 +241,7 @@ public class MainActivity extends Activity  {
             miMouseHandle = GLES20.glGetUniformLocation(program, "mouse");
 
             GLES20.glEnableVertexAttribArray(maPositionHandle);
-			
+
             mRectData.position(0);
             GLES20.glVertexAttribPointer(maPositionHandle, 2, GLES20.GL_FLOAT, false, 0, mRectData);
             mStartTime = SystemClock.elapsedRealtime();
@@ -256,11 +256,11 @@ public class MainActivity extends Activity  {
 			////
 			tempArray = new int[1];
 			GLES20.glGenFramebuffers(1, tempArray, 0);
-			//mFrameBufferObject = tempArray[0];
-// create fbo
+			mFrameBufferObject = tempArray[0];
+// create fbop
 
 		//	GLES20.glBindFramebuffer(GLES20.GL_FRAMEBUFFER, tempArray[0]);
-			mFrameBufferObject = tempArray[0];
+			//mFrameBufferObject = tempArray[0];
 
 
 			GLES20.glGenTextures(1, tempArray, 0);
@@ -296,13 +296,16 @@ public class MainActivity extends Activity  {
             GLES20.glUseProgram(program);
             GLES20.glVertexAttribPointer(0, 2, GLES20.GL_BYTE, false, 0, vertexBuffer);
 
-			GLES20.glDrawArrays(GLES20.GL_TRIANGLE_STRIP, 0, 4);	
-			
-			
+////			GLES20.glDrawArrays(GLES20.GL_TRIANGLE_STRIP, 0, 4);
+
+            GLES20.glUniform2fv(miMouseHandle, 1, mMouse, 0);
+            GLES20.glUniform2fv(miResolutionHandle, 1, mReducedResolution, 0);
+            long nowInSec = SystemClock.elapsedRealtime();
+            GLES20.glUniform1f(miGlobalTimeHandle, ((float) (nowInSec - mStartTime)) / 1000f);
 
 
             GLES20.glViewport(0, 0, (int)mReducedResolution[0], (int)mReducedResolution[1]);
-/*
+///*
 			GLES20.glBindFramebuffer(GLES20.GL_FRAMEBUFFER, mFrameBufferObject);
 
             GLES20.glDrawArrays(
@@ -318,23 +321,23 @@ public class MainActivity extends Activity  {
             GLES20.glViewport(0,0, (int)mResolution[0], (int)mResolution[1]);
 
             GLES20.glUseProgram(surfaceProgram);
-            GLES20.glVertexAttribPointer(surfacePositionLoc, 2, GLES20.GL_BYTE, false, 0, vertexBuffer);
-*/
+            GLES20.glVertexAttribPointer(surfacePositionLoc, 2, GLES20.GL_FLOAT, false, 0, mRectData);
+//*/
  /////           // :: uniforms
-            GLES20.glUniform2fv(miMouseHandle, 1, mMouse, 0);
-            GLES20.glUniform2fv(miResolutionHandle, 1, mReducedResolution, 0);
-            long nowInSec = SystemClock.elapsedRealtime();
-            GLES20.glUniform1f(miGlobalTimeHandle, ((float) (nowInSec - mStartTime)) / 1000f);
+            GLES20.glUniform2fv(
+                    surfaceResolutionLoc,
+                    1,
+                    surfaceResolution,
+                    0);
 
 			GLES20.glUniform1i(surfaceFrameLoc, 0);
-            // uniforms ::
+
 
             GLES20.glActiveTexture(GLES20.GL_TEXTURE0);
 
-            GLES20.glActiveTexture(GLES20.GL_TEXTURE0);
-            GLES20.glBindTexture(
-                    GLES20.GL_TEXTURE_2D,
-                    mTexture);
+  //          GLES20.glBindTexture(
+  //                  GLES20.GL_TEXTURE_2D,
+  //                      mTexture);
 
             GLES20.glClear(
                     GLES20.GL_COLOR_BUFFER_BIT);
